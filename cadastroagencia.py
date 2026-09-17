8fd090344aaf70df09738c1156e9b354e59cc84d
jotavecodex <vitor.rocha@dcomp.ufs.br>
2026-09-17 16:21:20 -0300

    cadastro das agências

---
+++ cadastroagencia.py
@@ -0,0 +1,15 @@
+agencia = [
+    "0001", "0002", "0003", "0004", "0005",
+    "0006", "0007", "0008", "0009", "0010",
+    "0011", "0012", "0013", "0014", "0015"
+]
+
+def cadastrar_agencia(codigo):
+    
+    if codigo in agencia:
+        return False  
+    elif len(codigo) == 4 and codigo.isdigit():
+        agencia.append(codigo)
+        return True   
+    else:
+        return False
