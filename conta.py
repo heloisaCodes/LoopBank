187b8884c65f96f1c6799da586e4164e73b1dbae
eduardasouza-dev <eduarda.souza@dcomp.ufs.br>
2026-09-24 14:36:06 -0300

    rafatorando conta para lista de tuplas

--- conta.py
+++ conta.py
@@ -1,15 +1,21 @@
 def criar_conta(numerodaconta, senha, cpf, agencia, saldo=0):
     clientes = []
     clientes.append(cpf)
-    conta = [agencia, numerodaconta, senha, saldo, clientes]
+
+    # conta agora vira TUPLA
+    conta = (agencia, numerodaconta, senha, saldo, clientes)
     return conta
 
+
 def cadastrarConta(contas, senha, cpf, agencia, saldo=0):
+    # numero da conta é gerada na hora
     numerodaconta = len(contas) + 1
+
+    # aqui vai ser criada a conta
     conta = criar_conta(numerodaconta, senha, cpf, agencia, saldo)
     contas.append(conta)
     return True
-# essa função ainda vai ser mudada, porque nao levei em consideraçao caso exclua o cliente que ja possui saldo
+
 
 def autenticar(conta, agenciadigitada, senhadigitada):
     if conta[0] != agenciadigitada:
@@ -19,12 +25,14 @@ def autenticar(conta, agenciadigitada, senhadigitada):
     else:
         return True
 
+
 def saque(saldoatual, valordosaque):
     if saldoatual < valordosaque:
         return saldoatual
     else:
         return saldoatual - valordosaque
 
+    
 def deposito(saldoatual, valordodeposito):
     if valordodeposito <= 0:
         return saldoatual
@@ -34,15 +42,32 @@ def deposito(saldoatual, valordodeposito):
 def listarContas(contas):
     return contas
 
+
 def transferir(contaOrigem, contaDestino, valor):
     if contaOrigem[3] < valor:
         return False
-    else:
-        contaOrigem[3] = contaOrigem[3] - valor
-        contaDestino[3] = contaDestino[3] + valor
-        return True
 
-    
+    # Como a conta é uma tupla, ent calcula os novos saldos e depois deixa tudo atualizad
+    novoSaldoOrigem = contaOrigem[3] - valor
+    novoSaldoDestino = contaDestino[3] + valor
+
+    novaContaOrigem = (
+        contaOrigem[0],
+        contaOrigem[1],
+        contaOrigem[2],
+        novoSaldoOrigem,
+        contaOrigem[4]
+    )
+
+    novaContaDestino = (
+        contaDestino[0],
+        contaDestino[1],
+        contaDestino[2],
+        novoSaldoDestino,
+        contaDestino[4]
+    )
+
+    return novaContaOrigem, novaContaDestino
 
 
 
