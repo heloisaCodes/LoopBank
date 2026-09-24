9df903415140ee82f0c4df648cd0c10d2a94ad58
eduardasouza-dev <eduarda.souza@dcomp.ufs.br>
2026-09-24 20:02:05 -0300

    adicionando comentário pra cada parte

--- conta.py
+++ conta.py
@@ -13,44 +13,48 @@ def cadastrarConta(contas, senha, cpf, agencia, saldo=0):
 
     # aqui vai ser criada a conta
     conta = criar_conta(numerodaconta, senha, cpf, agencia, saldo)
-    contas.append(conta)
+    contas.append(conta)  #adiciona conta na lista de contas
     return True
 
 
 def autenticar(conta, agenciadigitada, senhadigitada):
-    if conta[0] != agenciadigitada:
+    if conta[0] != agenciadigitada:  # porque conta[0] é agencia
         return False
-    elif conta[2] != senhadigitada:
+    elif conta[2] != senhadigitada:   # conta[2] é senha
         return False
     else:
         return True
-
-
-def saque(saldoatual, valordosaque):
+    
+# pra nao permitir sacar mais dinheiro do que o que tiver
+def saque(saldoatual, valordosaque):    
     if saldoatual < valordosaque:
         return saldoatual
     else:
         return saldoatual - valordosaque
 
-    
+# nao permite depósito de valor 0 ou negativo 
 def deposito(saldoatual, valordodeposito):
     if valordodeposito <= 0:
         return saldoatual
     else:
         return saldoatual + valordodeposito
 
-def listarContas(contas):
+
+def listarContas(contas): 
+    # apenas retorna a lista de contas
     return contas
 
 
 def transferir(contaOrigem, contaDestino, valor):
+    # ver se existe saldo suficiente
     if contaOrigem[3] < valor:
         return False
 
-    # Como a conta é uma tupla, ent calcula os novos saldos e depois deixa tudo atualizad
+    # como a conta é uma tupla, ent calcula os novos saldos e depois deixa tudo atualizado
     novoSaldoOrigem = contaOrigem[3] - valor
     novoSaldoDestino = contaDestino[3] + valor
 
+# novas tuplas com os saldos atualizados
     novaContaOrigem = (
         contaOrigem[0],
         contaOrigem[1],
@@ -68,7 +72,7 @@ def transferir(contaOrigem, contaDestino, valor):
     )
 
     return novaContaOrigem, novaContaDestino
-
+    # retorna as duas contas atualizadas
 
 
 
